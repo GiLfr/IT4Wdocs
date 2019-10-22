4f268680dd6b3defeb7e960be39936782bea0683
gilfr <glanteri@si2m.fr>
2019-10-22 10:25:25 +0100

    Deployed 094aab5 with MkDocs version: 1.0.4

---
+++ javascripts/addteams.js
@@ -0,0 +1,3 @@
+document.head.innerHTML += '<meta http-equiv="Content-Security-Policy" content="frame-ancestors teams.microsoft.com *.teams.microsoft.com *.skype.com">';
+document.body.innerHTML += 'microsoftTeams.initialize()';
+// document.head.innerHTML += '<link rel="stylesheet" type="text/css" href="styles.css" />';
